1aba7c789bed15c14d09c6a49cd05d54af6ebe45
Arjun Junna <arjun9852@gmail.com>
2022-01-13 11:08:10 +0530

    refactor: upgrade to es6

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-var txtInput = document.querySelector('#txt-input');
-var btnTranslate = document.querySelector('#btn-translate');
-var OutputDiv = document.querySelector('#txt-output');
+let txtInput = document.querySelector('#txt-input');
+let btnTranslate = document.querySelector('#btn-translate');
+let OutputDiv = document.querySelector('#txt-output');
 
-var serverURL = 'https://api.funtranslations.com/translate/minion.json';
+let serverURL = 'https://api.funtranslations.com/translate/minion.json';
 
-function getTranslationURL(input) {
-  return serverURL + '?' + 'text=' + input;
-}
+const getTranslationURL=(input)=>serverURL + '?' + 'text=' + input;
 
-function errorHandler(error) {
+
+const errorHandler=(error)=>{
   console.log('error occured', error);
   alert('Server down!! Try later');
 }
 
-function clickHandler() {
-  var inputText = txtInput.value;
+const clickHandler=()=>{
+  let inputText = txtInput.value;
 
   fetch(getTranslationURL(inputText))
     .then((response) => response.json())
